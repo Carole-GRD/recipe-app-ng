e46d72755b01f2d1ab5f8a4024eac62510c92efa
Carole Gérard <gerard.carole78@gmail.com>
2024-03-12 12:10:44 +0100

    Outputting a List of Recipes with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
 import { Recipe } from '../recipe.model';
+import { NgFor } from '@angular/common';
 
 
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
   imports: [
-    RecipeItemComponent
+    RecipeItemComponent,
+    NgFor
   ],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
@@ -16,6 +18,8 @@ import { Recipe } from '../recipe.model';
 
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.maxpixel.net/static/photo/1x/Recipe-Healthy-Food-Cooking-Delicious-Diet-Plate-753954.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+   
   ];
 }
