5d52c92952294543bad8fd991ce132f862e790b2
Carole Gérard <gerard.carole78@gmail.com>
2024-03-13 18:57:00 +0100

    Working on the ShoppingListComponent

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
 
+
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
@@ -10,6 +11,8 @@ import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent {
 
+
+export class ShoppingListComponent {
+  ingredients = [];
 }
