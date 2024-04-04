9d0b81407fff77607792860d4c6350dbd85d71bb
Carole Gérard <gerard.carole78@gmail.com>
2024-04-04 11:58:19 +0200

    Building and Using a Dropdown Directive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { DropdownDirective } from '../shared/dropdown.directive';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [DropdownDirective],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { DropdownDirective } from '../../shared/dropdown.directive';
 
 
 @Component({
   selector: 'app-recipe-detail',
   standalone: true,
-  imports: [],
+  imports: [DropdownDirective],
   templateUrl: './recipe-detail.component.html',
-  styleUrl: './recipe-detail.component.css'
+  styleUrl: './recipe-detail.component.css',
 })
 
 
